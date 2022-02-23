81664eb31cbd12788dbd0d4c27eed8f12d16c13b
[rrenat358] <[rrenat358@gmail.com]>
2022-02-23 22:02:58 +0300

    v.20220223-2102

--- src/rrenat358/WindowGame.java
+++ src/rrenat358/WindowGame.java
@@ -4,17 +4,31 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 
+//==================================================
 public class WindowGame extends JFrame {
     private static WindowGame windowGame;
     private static Image background;
-    private static Image gameOver;
-    private static Image drop;
 
+    private static Image drop;
+    private static float dropLeft=200;
+    private static float dropTop=-100;
+    private static float dropV=200;
+
+
+    private static long lastFrameTime=System.nanoTime();
+
+
+
+    private static Image gameOver;
+
+
+//==================================================
     public static void main(String[] args) throws IOException {
-        background = ImageIO.read(WindowGame.class.getResourceAsStream("images/Sky-01.jpg"));
-        gameOver = ImageIO.read(WindowGame.class.getResourceAsStream("images/GAME-OVER-02.png"));
-        drop = ImageIO.read(WindowGame.class.getResourceAsStream("images/drop.03.png"));
+        background = ImageIO.read(Objects.requireNonNull(WindowGame.class.getResourceAsStream("images/Sky-01.jpg")));
+        gameOver = ImageIO.read(Objects.requireNonNull(WindowGame.class.getResourceAsStream("images/GAME-OVER-02.png")));
+        drop = ImageIO.read(Objects.requireNonNull(WindowGame.class.getResourceAsStream("images/drop.03.png")));
 
         windowGame = new WindowGame(); //создаём объект WindowGame()
         //настройка окна ↓
@@ -23,24 +37,30 @@ public class WindowGame extends JFrame {
         windowGame.setSize(1200,800);
         windowGame.setResizable(false);
 
-
         FieldGame fieldGame = new FieldGame(); //создаём объект класса fieldGame
         windowGame.add(fieldGame); //рисуем в нашем окне
 
         windowGame.setVisible(true);
-
-
     }
 
+//==================================================
     //новый метод для рисования
     public static void onRepaint(Graphics g){
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime - lastFrameTime) * 0.000000001f;
+        lastFrameTime = currentTime;
+
+
+        dropTop = dropTop + dropV * deltaTime;
         g.drawImage(background, 0, 0, null);
-        g.drawImage(drop, 100, 100, null);
-        g.drawImage(gameOver, 330, 130, null);
+        g.drawImage(drop, (int) dropLeft, (int) dropTop, null);
+
+
+//        g.drawImage(gameOver, 330, 130, null);
 //        g.fillOval(10,10,200,100);
 
     }
-
+//==================================================
     //новый класс для панелей для рисования
     private static class FieldGame extends JPanel {
 
@@ -51,9 +71,10 @@ public class WindowGame extends JFrame {
             //сново получаем доступ к JPanel
             super.paintComponent(g); //отрисовка панели
             onRepaint(g);
+            repaint();
 
         }
-
+//==================================================
 
 
     }
